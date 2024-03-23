9004edf202a41bcda44c856f82b15a10d024e938
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-03-23 22:03:15 +0300

    Added a function for the button element and initialized variables

---
+++ student-grade-generator/script.js
@@ -0,0 +1,5 @@
+function calculateGrade() {
+    const marksInput = document.getElementById("marks");
+    const marks = parseInt(marksInput.value);
+    const gradeResult = document.getElementById("grade-result");
+}
