05c62c2d8f1fe17ea31c0a3f5bdcecbc95ef4e11
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-03-24 13:11:57 +0300

    Added conditions in js

--- speed-detector/script.js
+++ speed-detector/script.js
@@ -4,3 +4,24 @@ const pointsElement = document.getElementById("result");
 
 function checkSpeed() {
   const carSpeed = parseInt(document.getElementById("carSpeed").value);
+
+  if (isNaN(carSpeed)) {
+    pointsElement.textContent = "Please enter a valid number.";
+    return;
+  }
+
+  const deductionPoints = Math.max(
+    0,
+    Math.floor((carSpeed - speedLimit) / demeritPerKm)
+  );
+
+  if (carSpeed <= speedLimit) {
+    pointsElement.textContent = "Ok";
+  } else if (deductionPoints > 12) {
+    pointsElement.textContent = "License suspended";
+  } else {
+    pointsElement.textContent = `Points: ${deductionPoints}`;
+  }
+}
+
+
